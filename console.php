2007dab217fec89a92f42ea373bb580f7d50841d
Benoît Mallo <benoit.mallo@gmail.com>
2017-09-23 12:01:42 +0000

    Add d'une nouvelle commande pour créer un nouveau module.

--- console.php
+++ console.php
@@ -32,8 +32,72 @@ $app->command('server [port]', function ($port, OutputInterface $output) {
     $output->writeln("Le server écoute sur le port {$port}");
 })->defaults(['port' => 8000]);
 
-$app->command('new:application name', function (string $name, OutputInterface $output) {
-    // @TODO Cela va créer le dossier du "module" avec les bon fichiers et dossier automatiquement.
+/**
+ * Permet de ce créer un nouveau module + en créant l'Application.php et le dossier des vues (views).
+ * @param $name string Le nom de votre module.
+ *
+ * @example php console.php application:new Demo
+ */
+$app->command('application:new name', function (string $name, OutputInterface $output) {
+    $name = ucfirst($name);
+    $applicationDir = __DIR__ . '/src/Application/' . $name;
+    $twigNamespace = strtolower($name);
+    if (!is_dir($applicationDir)) {
+        mkdir($applicationDir,  0755, true);
+    }
+    if (!is_dir($applicationDir . '/views')) {
+        mkdir($applicationDir . '/views',  0755, true);
+    }
+    $routerParam = '$router';
+    file_put_contents($applicationDir . '/Application.php', <<<EOT
+<?php
+namespace App\\$name;
+
+use TuxBoy\ApplicationInterface;
+use TuxBoy\Router\Router;
+use function DI\add;
+
+/**
+ * The $name application
+ */
+class Application implements ApplicationInterface
+{
+    /**
+     * Définie les routes de l'application.
+     *
+     * @param Router $routerParam
+     */
+    public function getRoutes(Router $routerParam): void
+    {
+        // ...
+    }
+
+    /**
+     * Pour ajouter la configuration au container de son application.
+     *
+     * @return array
+     */
+    public function addConfig(): array
+    {
+        return [
+            'twig.path' => add([
+               $twigNamespace  => __DIR__ . '/views/'
+            ]),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return '$name';
+    }
+}
+
+EOT
+    );
+    $output->writeln("Votre application $name a été créé");
 });
 
 $app->command('migrate [force]', function ($force, OutputInterface $output) {
